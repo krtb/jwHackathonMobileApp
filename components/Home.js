f9b50df6fc2592974ccc96670d35d55e356a3bea
Kurt Bauer <kbauer@KBauer-MBP-13.home>
2020-08-14 21:58:11 -0400

    create Home functional component

---
+++ components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, ScrollView, View } from 'react-native';
+
+const Home = () => {
+    return (
+        <View >
+           <Text> Home </Text>
+        </View>
+    )
+}
+
+export default Home
