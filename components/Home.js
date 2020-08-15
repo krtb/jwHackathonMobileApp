9e270ad1acf3f287cd1f17352e01994d29a84911
Kurt Bauer <kbauer@KBauer-MBP-13.home>
2020-08-14 23:43:31 -0400

    navigate to videos and players from home

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const Home = () => {
+const Home = ({navigation}) => {
     return (
         <View >
            <Text> Home </Text>
+            <Button
+                title="View Your Videos Playlist"
+                onPress={() => navigation.navigate('VideosList')}
+            />
+            <Button
+                title="View Your Players Playlist"
+                onPress={() => navigation.navigate('PlayersList')}
+            />
         </View>
     )
 }
